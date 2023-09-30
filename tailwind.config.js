0bd140a95e3a6334f3ab326e7168b9e271990dbd
Agnieszka26 <agna.medrek@gmail.com>
2023-09-30 16:54:10 +0200

    ci: add mui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    colors:{
+      blue: '#00d1cd'
+    },
+   fontFamily: {
+    chivo: 'Chivo',
+    sigmar: 'Sigmar One',
+  }
   },
   plugins: [],
-}
-
+};
