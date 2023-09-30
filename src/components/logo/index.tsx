2af98bdf104a4df7bb636461f552b2a4db6d0509
Agnieszka26 <agna.medrek@gmail.com>
2023-09-30 18:21:26 +0200

    feat: add logo component

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,15 +1,17 @@
 'use client';
 import { Typography } from '@mui/material';
+import Image from "next/image"
 import { FC } from 'react';
+
+import logo_icon from "@/assets/icons/logos/logo-icon-blue.png"
 interface LogoProps {}
 
 const Logo: FC<LogoProps> = () => {
   return (
     <Typography
-    
-     className="text-white"
-    >
-      LOGO
+    component="button"
+        >
+     <Image src={logo_icon} alt="logo join code" height={46}/>
     </Typography>
   );
 };
