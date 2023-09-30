74c35403df911cb17476e103d10a3b96d271f861
Agnieszka26 <agna.medrek@gmail.com>
2023-09-30 11:03:19 +0200

    test: configure and run first e2e and component tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,18 @@
-import {defineConfig} from "cypress"
+import { defineConfig } from "cypress";
 
 module.exports = defineConfig({
-    projectId: "xe16wz",
-})
+  projectId: "xe16wz",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
